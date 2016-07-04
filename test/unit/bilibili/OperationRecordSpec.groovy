d87d722737bdb04b921e79eca502d4edd9dd3619
LDZ <827489423@qq.com>
2016-07-04 16:25:57 +0800

    文件上传

---
+++ test/unit/bilibili/OperationRecordSpec.groovy
@@ -0,0 +1,20 @@
+package bilibili
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(OperationRecord)
+class OperationRecordSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
---
+++ test/unit/bilibili/PublicTagLibSpec.groovy
@@ -0,0 +1,20 @@
+package bilibili
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(PublicTagLib)
+class PublicTagLibSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
